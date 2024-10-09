91ff83fdd68054a956b12084c58bcd8fa2dfd198
Gregory Douglas <2968519+ggdouglas@users.noreply.github.com>
2024-10-09 14:08:43 -0400

    Sandpack!

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,52 @@
-import { useState } from "react";
-import blueprintLogo from "./assets/blueprint.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
-import { Button, Intent } from "@blueprintjs/core";
+import { Sandpack } from "@codesandbox/sandpack-react";
+
+const app = `import { Button } from '@blueprintjs/core'
+
+export default function App() {
+  return (
+    <Button intent="primary">
+      Hello World
+    </Button>
+  );
+}`;
+
+const index = `import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { FocusStyleManager } from "@blueprintjs/core";
+import "./styles.css";
+
+import App from "./App";
+import React from "react";
+
+import "@blueprintjs/core/lib/css/blueprint.css";
+
+FocusStyleManager.onlyShowFocusOnTabs();
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+`;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://blueprintjs.com" target="_blank">
-          <img
-            src={blueprintLogo}
-            className="logo blueprint"
-            alt="Blueprint logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + Blueprint</h1>
-      <div className="card">
-        <Button
-          intent={Intent.PRIMARY}
-          onClick={() => setCount((count) => count + 1)}
-        >
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and Blueprint logos to learn more
-      </p>
-    </>
+    <Sandpack
+      template="react-ts"
+      theme="auto"
+      options={{ visibleFiles: ["/App.tsx"] }}
+      customSetup={{
+        dependencies: {
+          "@blueprintjs/core": "^5.13.1",
+        },
+      }}
+      files={{
+        "/App.tsx": app,
+        "/index.tsx": index,
+      }}
+    />
   );
 }
 
