567ace5fb15110f58f8521f40ccbf28d0a190a2c
Gregory Douglas <2968519+ggdouglas@users.noreply.github.com>
2024-10-09 10:23:42 -0400

    Use modern compiler for sass

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
